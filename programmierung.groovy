d2014acf53cbf07ef68c537619ab955de99f3219
Jens Bendisposto <jens@bendisposto.de>
2017-10-07 07:55:18 +0200

    Create programmierung.groovy

---
+++ programmierung.groovy
@@ -0,0 +1,24 @@
+apply plugin: 'java'
+
+sourceSets.main.java.srcDir('.')
+sourceSets.main.output.classesDir('bin')
+
+repositories { mavenCentral(); }
+
+def java(String x) {
+  def arguments = x.split(" ")
+  def t = arguments.tail() as List
+  new ByteArrayOutputStream().withStream { os ->
+    javaexec {
+      main = arguments.head()
+      classpath = sourceSets.main.runtimeClasspath
+      args = t
+      standardOutput = os
+    }
+    os.toString();
+  }
+}
+
+ext {
+  java = this.&java
+}
