f035a4ac6046ed6f2a670278cff934475d683a1b
Jens Bendisposto <jens@bendisposto.de>
2017-11-09 08:43:51 +0100

    Update programmierung.groovy

--- programmierung.groovy
+++ programmierung.groovy
@@ -5,15 +5,17 @@ sourceSets.main.output.classesDir('klassen')
 
 repositories { mavenCentral(); }
 
-def java(String x) {
+def java(String x, String i = "") {
   def arguments = x.split(" ")
   def t = arguments.tail() as List
+  def ins = new ByteArrayInputStream(i.getBytes())
   new ByteArrayOutputStream().withStream { os ->
     javaexec {
       main = arguments.head()
       classpath = sourceSets.main.runtimeClasspath
       args = t
       standardOutput = os
+      standardInput = ins
     }
     os.toString();
   }
